909408b15448a046b9f806f1589ff4165b8de2b4
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 19:53:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,10 @@ def oxford_comma(array)
   elsif array.size == 2
     output = "#{array[0]} and #{array[1]}"
   else
-    
+    for index in 0..array.size - 2 do
+      output += "#{array[index], }"
+    end
+    output += "and #{array.last}"
   end
+  output
 end
