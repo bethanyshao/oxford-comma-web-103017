e51169c609d8dfe1cf729cd4d6e18ca95449b146
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 19:25:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.size == 1
+    output = array.first
+  elsif array.size == 2
+    output = "#{array[0]} and #{array[1]}"
+  else
+    
+  end
 end
